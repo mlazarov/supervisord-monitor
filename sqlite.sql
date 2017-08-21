8b2da9f7bd928cbf2cfbb9d32698987472c22e94
feix <feix.chow@gmail.com>
2017-08-21 23:19:27 +0800

    1. Using sqlite db for servers management 2. Manipulating server info on web page

---
+++ sqlite.sql
@@ -0,0 +1,6 @@
+create table servers (name TEXT PRIMARY KEY NOT NULL, ip TEXT NOT NULL, port TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL);
+/*
+insert into servers (name, ip, port, username, password) values ('server01', 'server01.app', 9001, 'yourusername', 'yourpass');
+insert into servers (name, ip, port, username, password) values ('server02', 'server02.app', 9001, '', '');
+insert into servers (name, ip, port, username, password) values ('server03', 'server03.app', 9001, '', '');
+*/
